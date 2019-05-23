907aa344d701086c9fbd7c82a6ea1c24458980ae
Mateus Rocha <mateusrochati@gmail.com>
2019-05-22 21:53:45 -0300

    Vue-Component commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<app-menu />
+		<app-toolbar />
+		<v-content>
+			<app-carrosel/>	
+		</v-content>	
+		<app-footer />
+	</v-app>
+</template>
+
+<script>
+import Footer from '@/Footer.vue'
+import Carrosel from '@/Carousel.vue'
+import ToolBar from '@/Toolbar'
+import Menu from '@/Menu'
+
+export default {
+
+	components: {"app-footer": Footer,
+				"app-carrosel": Carrosel,
+				"app-toolbar": ToolBar,
+				"app-menu": Menu}
+	
+
+}
+</script>
---
+++ src/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Usando-</span>
+				<span class="font-weight-light">Componente</span>
+			</v-toolbar-title>
+		</v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
